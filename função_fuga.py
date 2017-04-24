51d105e60070c4206645a7e43544685073f1dbb6
leonardoimakuma <leonardonnneves@gmail.com>
2017-04-24 20:21:56 -0300

    função fuga
    função para fugir das batalhas baseada em sorte

---
+++ função_fuga.py
@@ -0,0 +1,10 @@
+import random
+lista_fuga = [1,2,3,4,5,6,7,8,9,10]
+
+if atacar_fugir == "fugir" :
+            fuga = random.choice(lista_fuga)
+            if fuga > 2:
+                print("Você conseguiu escapar desse inspermon selvagem!")
+                break
+            else:
+                print("Ixi , hoje não é o seu dia de sorte :( ")
